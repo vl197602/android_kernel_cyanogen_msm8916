fea1060f28d39ce10a3631d4e0647c4ed5c7d554
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-05-14 22:32:52 +0700

    radio: iris: More checking for NULL pointer access
    Unregister radio device from FM_OFF mode crashes the kernel
    
    Change-Id: I1c18546b9394fdab8ccbb387850629037e636239
    
    Conflicts:
    	drivers/media/radio/radio-iris-transport.c

--- drivers/media/radio/radio-iris-transport.c
+++ drivers/media/radio/radio-iris-transport.c
@@ -40,7 +40,7 @@ struct radio_data {
 struct radio_data hs;
 #ifndef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
 static DEFINE_MUTEX(fm_smd_enable);
-static int fmsmd_set;
+static int fmsmd_set = 0;
 static int hcismd_fm_set_enable(const char *val, struct kernel_param *kp);
 module_param_call(fmsmd_set, hcismd_fm_set_enable, NULL, &fmsmd_set, 0644);
 static void radio_hci_smd_deregister(void);
@@ -214,6 +214,11 @@ void radio_hci_smd_deregister(void)
 static void radio_hci_smd_deregister(void)
 #endif
 {
+	struct radio_data *hsmd = &hs;
+
+	if (hsmd == NULL)
+		goto done;
+
 	if (hs.hdev != NULL) {
 		radio_hci_unregister_dev(hs.hdev);
 		kfree(hs.hdev);
@@ -222,6 +227,7 @@ static void radio_hci_smd_deregister(void)
 
 	smd_close(hs.fm_channel);
 	hs.fm_channel = 0;
+done:
 #ifdef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
 	fmsmd_ready = -1;
 #else
